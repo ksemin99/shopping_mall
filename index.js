5edcf45211715d32f63158d57059986b00586061
qsc0415 <qsc0415@naver.com>
2022-09-16 04:43:31 +0900

    220916_JWT관리 및 회원가입 체크
    React 이게 맞는지 모르겠다

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
+const PORT = 3000;
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
 //const dotenv = require('dotenv').config(); // #1
 const mysqlConObj = require('./config/mysql'); // #2
@@ -10,12 +13,51 @@ const db = mysqlConObj.init();
 https: app.use(express.json());
 app.use(cors());
 
-const users = [];
+dotenv.config();
 
-app.get('/user', function (req, res) {
+// 이 아래 부분이 DB로 받는것들
+const users = [
+  {
+    id: 'Park',
+    pw: 'pw1',
+  },
+  {
+    id: 'Kim',
+    pw: 'pw2',
+  },
+];
+
+app.post('/login', (req, res, next) => {
+  const { id, pw } = req.body;
+
+  const user = {
+    id: id,
+    pw: pw,
+  };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.get('/user', authenticateToken, (req, res) => {
+  res.json(
+    users.filter((user) => user.id === req.user.id && user.pw === req.user.pw)
+  );
   // 첫번째 인자 req: 클라이언트에서 요청이올 때, ReqBody, ReqHeader, url 등등 그런 정보들이 모두 들어있다.
   // 두번째 인자 res: 클라이언트에 응답할 때 필요한 모든 정보들이 들어있다. 지금부터 저희가 작성할 내용 외에도 기본적으로 들어가야되는 네트워크 정보라던지 그런 것들이 모두 여기 들어있다.
-  res.send({ users: users });
+  //res.send({ users: users });
 });
 
 app.post('/user', function (req, res) {
@@ -24,6 +66,31 @@ app.post('/user', function (req, res) {
   return res.send({ sucess: true });
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('server listening on port 3000');
+});
+
+app.post('/newuser', function (req, res) {
+  const id = req.body.id;
+  const pw = req.body.pw;
+  const pwchk = req.body.pwchk;
+  const { name, sex } = req.body;
+  if (id.replace(/(\s*)/g, '').length >= 4) {
+    // DB에서 id 중복확인
+    if (
+      pw.replace(/(\s*)/g, '').length >= 8 &&
+      pw.replace(/(\s*)/g, '').length <= 20
+    ) {
+      if (pw === pwchk) {
+        res.send('굳');
+        //회원가입 성공 + DB에 저장
+      } else {
+        res.send('꺼져');
+      }
+    } else {
+      res.send('꺼져');
+    }
+  } else {
+    res.send('꺼져');
+  }
 });

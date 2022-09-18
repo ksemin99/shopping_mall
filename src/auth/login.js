061286ccc005ee4b66eef7ddb6ee962ef31c281f
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-09-19 00:01:47 +0900

    login시 데베 안에 아이디 확인

--- src/auth/login.js
+++ src/auth/login.js
@@ -21,12 +21,33 @@ app.post('/', (req, res, next) => {
 
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET); //이거는 DB에 저장
-  sql =
-    "UPDATE users SET token ='" + refreshToken + "' where id ='" + id + "';"; // refreshtoken DB에 저장
-  db.query(sql, (err, result) => {
+
+  //id 체크부분 --
+  checkidsql =
+    "SELECT EXISTS (select * from users where id = '" + id + "') as isChk";
+  db.query(checkidsql, (err, result) => {
     if (err) console.log(err);
-    else console.log(result, 'mysql 로그인 성공');
+    else {
+      console.log(result[0].isChk);
+      checkid = result[0].isChk;
+      if (checkid == 0) {
+        console.log('db 안에 아이디 없음');
+      } else {
+        sql =
+          "UPDATE users SET token ='" +
+          refreshToken +
+          "' where id ='" +
+          id +
+          "';"; // refreshtoken DB에 저장
+        db.query(sql, (err, result) => {
+          if (err) console.log(err);
+          else console.log(result, 'mysql 안에 access 토큰 삽입 성공');
+        });
+      }
+    }
   });
+  //--
+
   res.json({ accessToken: accessToken, refreshToken: refreshToken }); //이 부분이 로그인 시 accesstoken이랑 refreshtoken 나오는 곳
 });
 function generateAccessToken(user) {

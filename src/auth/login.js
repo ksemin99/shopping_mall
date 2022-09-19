da706190b7eb8ffde3f17b83d45487ce6c92faec
qsc0415 <qsc0415@naver.com>
2022-09-19 23:45:19 +0900

    220919_아이디 콘솔 찍기

--- src/auth/login.js
+++ src/auth/login.js
@@ -49,6 +49,7 @@ app.post('/', (req, res, next) => {
           if (err) console.log(err);
           else {
             idIndex = result[0].u_num;
+            console.log(idIndex);
             console.log('u_num : ' + result[0].u_num);
           }
         });

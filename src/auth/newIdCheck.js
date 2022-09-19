2afedd2bc64cb03ca13b5a2baf73d350ee3e2bcc
ksemin99 <70834817+ksemin99@users.noreply.github.com>
2022-09-20 01:04:51 +0900

    test7

--- src/auth/newIdCheck.js
+++ src/auth/newIdCheck.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 dotenv.config();
 
-app.get(`/:${userid}`, (req, res, next) => {
+app.get('/:${userid}', (req, res, next) => {
   const userid = req.params.userid;
   console.log(userid + 'ddd');
   checkidsql =

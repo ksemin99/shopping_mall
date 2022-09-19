3cfa729a4f5115dedfae5d28a1080d9b3e9dbfdf
qsc0415 <qsc0415@naver.com>
2022-09-20 01:30:00 +0900

    원상

--- src/auth/newIdCheck.js
+++ src/auth/newIdCheck.js
@@ -11,15 +11,16 @@ app.use(cors());
 
 dotenv.config();
 
-app.get('/:id', (req, res, next) => {
-  const id = req.params.id;
-  console.log(id + ' ddd');
+app.get('/:userId', (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(req.params);
+  console.log(userId + ' ddd');
   checkidsql =
-    "SELECT EXISTS (select * from users where id = '" + id + "') as isChk";
+    "SELECT EXISTS (select * from users where id = '" + userId + "') as isChk";
   db.query(checkidsql, (err, idresult) => {
     if (err) console.log(err);
     else {
-      console.log(id);
+      console.log(userId);
       if (idresult[0].isChk == 1) {
         res.send('아이디가 데베에 존재함');
       } else {

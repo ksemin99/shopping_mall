aaef04de5e35ef2e9ce6a77db5cd2888d9b8e2cf
qsc0415 <qsc0415@naver.com>
2022-09-25 05:30:15 +0900

    Update category.js

--- src/main/category.js
+++ src/main/category.js
@@ -19,11 +19,21 @@ app.get('/', (req, res, next) => {
     data: [
       {
         categoryid: 0,
+        categoryname: 'NEW',
+        subcategory: ['신상품 매일 자정 오픈'],
+      },
+      {
+        categoryid: 1,
+        categoryname: 'BEST',
+        subcategory: ['가장 많이 조회한 상품'],
+      },
+      {
+        categoryid: 2,
         categoryname: 'TOP',
         subcategory: ['맨투맨 & 후디 / ', '블라우스 & 셔츠 / ', '니트'],
       },
       {
-        categoryid: 1,
+        categoryid: 3,
         categoryname: 'PANTS',
         subcategory: [
           '배기 / ',
@@ -35,17 +45,17 @@ app.get('/', (req, res, next) => {
         ],
       },
       {
-        categoryid: 2,
+        categoryid: 4,
         categoryname: 'OUTER',
         subcategory: ['코트 / ', '자켓 / ', '점퍼 / ', '가디건 / ', '조끼'],
       },
       {
-        categoryid: 3,
+        categoryid: 5,
         categoryname: 'SKIRT',
         subcategory: ['치마 / ', '원피스'],
       },
       {
-        categoryid: 4,
+        categoryid: 6,
         categoryname: 'SHOES&BAGS',
         subcategory: ['신발 / ', '가방'],
       },

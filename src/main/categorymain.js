29ac196455cd49fcd662c0cc8e178beeb5158bdb
qsc0415 <qsc0415@naver.com>
2022-09-25 04:43:57 +0900

    q

---
+++ src/main/categorymain.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const checkauthorization = require('../../auth/checkauthorization');
+const mainfunction = require('../mainfunction');
+
+const mysqlConObj = require('../../../config/mysql'); // #2
+const { request } = require('express');
+const db = mysqlConObj.init();
+
+app.use(cors());
+
+dotenv.config();
+
+app.get('/:categoryid', (req, res, next) => {
+  // DB로 categoryid 별 애들 불러오기
+  res.send(userstat);
+});
+
+module.exports = app;

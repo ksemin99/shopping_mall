3ffbd57387ee87971e88f0222e328f4f827f4ee5
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-09-25 02:36:20 +0900

    Update top.js

--- src/main/maincategory/top.js
+++ src/main/maincategory/top.js
@@ -25,7 +25,7 @@ app.get(
     let userstat = '';
     switch (subcategory) {
       case 0:
-        console.log('top'); 1
+        console.log('top');
         if (req.userstat == '') userstat = 'top 연결 완료';
         else userstat = req.userstat;
         break;

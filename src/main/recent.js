3ec57183e08d70bb1ba6b4c4d48cac7b5f31c2d1
qsc0415 <qsc0415@naver.com>
2022-09-26 02:01:06 +0900

    Update recent.js

--- src/main/recent.js
+++ src/main/recent.js
@@ -16,34 +16,34 @@ dotenv.config();
 
 app.get('/', (req, res, next) => {
   //new랑 best랑 카테고리별 4개씩
-  mainsql =
+  newsql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num order by b.b_time desc limit 12';
-  mainsql =
+  bestsql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num order by b.b_views desc limit 12';
-  mainsql =
+  topsql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 1 limit 12';
-  mainsql =
+  pantssql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 2 limit 12';
-  mainsql =
+  outersql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 3 limit 12';
-  mainsql =
+  skirtsql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 4 limit 12';
-  mainsql =
+  shoesbagssql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 5 limit 12';
-  db.query(mainsql, (err, result) => {
-    if (err) console.log(err);
-    else console.log(result, 'sql 성공');
-    res.send(result);
-  });
-
-  // res.send({
-  //   picture: '',
-  //   color: '',
-  //   dressname: '',
-  //   dressprice: '',
-  //   views: '',
-  //   size: '',
-  // });
+  db.query(
+    newsql,
+    bestsql,
+    topsql,
+    pantssql,
+    outersql,
+    skirtsql,
+    shoesbagssql,
+    (err, result) => {
+      if (err) console.log(err);
+      else console.log(result, 'sql 성공');
+      res.send(result);
+    }
+  );
 });
 
 module.exports = app;

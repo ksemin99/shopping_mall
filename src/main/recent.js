96165d524061b46363ccd9054bea301eb1cfd029
qsc0415 <qsc0415@naver.com>
2022-09-25 19:25:14 +0900

    Update recent.js

--- src/main/recent.js
+++ src/main/recent.js
@@ -15,20 +15,24 @@ app.use(cors());
 dotenv.config();
 
 app.get('/', (req, res, next) => {
-  mainsql = ''; //카테고리별 4개씩 들고오기
+  mainsql =
+    'SELECT category.c_name, board.b_name, board.b_price, board.b_color, board.b_size FROM category join board on category.c_num = board.c_num AND ROWNUM < 5'; //카테고리별 4개씩 들고오기
 
   db.query(mainsql, (err, result) => {
     if (err) console.log(err);
     else console.log(result, 'sql 성공');
   });
 
-  res.send({
-    picture: '',
-    color: '',
-    dressname: '',
-    dressprice: '',
-    views: '',
-  });
+  res.send(result);
+
+  // res.send({
+  //   picture: '',
+  //   color: '',
+  //   dressname: '',
+  //   dressprice: '',
+  //   views: '',
+  //   size: '',
+  // });
 });
 
 module.exports = app;

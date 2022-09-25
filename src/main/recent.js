6288829b9e030485259c22e1b39be635f3c5b425
qsc0415 <qsc0415@naver.com>
2022-09-26 02:08:04 +0900

    Update recent.js

--- src/main/recent.js
+++ src/main/recent.js
@@ -16,6 +16,7 @@ dotenv.config();
 
 app.get('/', (req, res, next) => {
   //new랑 best랑 카테고리별 4개씩
+  let recentresult = [];
   newsql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num order by b.b_time desc limit 12';
   bestsql =
@@ -30,20 +31,42 @@ app.get('/', (req, res, next) => {
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 4 limit 12';
   shoesbagssql =
     'select b.b_name, b.b_url, b.b_price, bd.b_color, b.b_views from board b, board_detail bd where b.b_num = bd.b_num and b.c_num = 5 limit 12';
-  db.query(
-    newsql,
-    bestsql,
-    topsql,
-    pantssql,
-    outersql,
-    skirtsql,
-    shoesbagssql,
-    (err, result) => {
-      if (err) console.log(err);
-      else console.log(result, 'sql 성공');
-      res.send(result);
-    }
-  );
+  db.query(newsql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    recentresult.push(result);
+    res.send(result);
+  });
+  db.query(bestsql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    res.send(result);
+  });
+  db.query(topsql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    res.send(result);
+  });
+  db.query(pantssql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    res.send(result);
+  });
+  db.query(outersql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    res.send(result);
+  });
+  db.query(skirtsql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    res.send(result);
+  });
+  db.query(shoesbagssql, (err, result) => {
+    if (err) console.log(err);
+    else console.log(result, 'sql 성공');
+    res.send(result);
+  });
 });
 
 module.exports = app;

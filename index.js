32f69b621db1e48f5b114a6899d1eaa7b45dc23f
qsc0415 <qsc0415@naver.com>
2022-09-26 05:17:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,13 +63,12 @@ app.get('/', (req, res, next) => {
   db.query(testsql2, (err, result) => {
     if (err) console.log(err);
     else {
-      for (let data of result) {
-        semi.push(data.b_color);
-      }
-      console.log(JSON.parse(JSON.stringify(result)));
-      //semi = Object.values(JSON.parse(JSON.stringify(result)));
-      console.log(semi);
-      sqlresult.data1[0].b_color = semi;
+      // for (let data of result) {
+      //   semi.push(data.b_color);
+      // }
+      // console.log(semi);
+      // sqlresult.data1[0].b_color = semi;
+      sqlresult.data1[0].b_color = result;
       res.send(sqlresult);
     }
   });

9a507ccf1f62f3698e1c5cde125f069e1cebc6ee
qsc0415 <qsc0415@naver.com>
2022-09-27 09:07:51 +0900

    Update recent.js

--- src/main/recent.js
+++ src/main/recent.js
@@ -21,15 +21,15 @@ app.get('/', (req, res, next) => {
   bestsql =
     'SELECT DISTINCT b.b_name, b.b_url, b.b_price, b.b_views FROM board b, board_color bc WHERE b.b_num = bc.bc_num ORDER BY b.b_views desc limit 4';
   topsql =
-    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 1 and ORDER BY b.b_views desc limit 4';
+    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 1 ORDER BY b.b_views desc limit 4';
   pantssql =
-    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 2 and ORDER BY b.b_views desc limit 4';
+    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 2 ORDER BY b.b_views desc limit 4';
   outersql =
-    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 3 and ORDER BY b.b_views desc limit 4';
+    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 3 ORDER BY b.b_views desc limit 4';
   skirtsql =
-    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 4 and ORDER BY b.b_views desc limit 4';
+    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 4 ORDER BY b.b_views desc limit 4';
   shoesbagssql =
-    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 5 and ORDER BY b.b_views desc limit 4';
+    'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 5 ORDER BY b.b_views desc limit 4';
 
   db.query(bestsql, (err, result) => {
     if (err) console.log(err);

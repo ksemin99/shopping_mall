602983deee64ade89a4afb37e6048cab426968a8
qsc0415 <qsc0415@naver.com>
2022-09-29 03:21:27 +0900

    Update categoryrecent.js

--- src/main/categoryrecent.js
+++ src/main/categoryrecent.js
@@ -14,84 +14,88 @@ app.use(cors());
 
 dotenv.config();
 
-app.get('/:categoryid', (req, res, next) => {
-  let categorysql = '';
+app.get('/', (req, res, next) => {
+  const categoryid = Number(req.query.categoryid); // 카테고리 ID //
+  const search = req.query.search;
+  const page = req.query.page; //limit ( (page - 1) * size  , 1 )
+  const size = req.query.size; //limit ( (page - 1) * size  , 1 )
+  const pullsort = req.query.sort;
+
+  const splitresult = pullsort.split(',');
+
+  const sort = splitresult[0]; //낮은가격, 높은가격, 최신순, 조회순//
+  const standard = splitresult[1]; //
+
   // DB로 categoryid 별 애들 불러오기
-  const categoryid = Number(req.params.categoryid);
-  //const page = req.params.page;
+
   console.log(categoryid);
-  let j = 19;
+
   switch (categoryid) {
-    case 0: //new 카테고리 별로 제일 최근꺼 그냥 쫙
-      categorysql =
-        'SELECT DISTINCT b.b_name, b.b_url, b.b_price, b.b_views, b.b_time FROM board b, board_color bc WHERE b.b_num = bc.bc_num ORDER BY b.b_time desc';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board ORDER BY b_time desc limit ' +
-        j +
-        ', 1)';
-      break;
-    case 1: //best 조회수 높은 순
-      categorysql =
-        'SELECT DISTINCT b.b_name, b.b_url, b.b_price, b.b_views FROM board b, board_color bc WHERE b.b_num = bc.bc_num ORDER BY b.b_views desc';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board ORDER BY b_views desc limit ' +
-        j +
-        ', 1)';
-      break;
-    case 2: //top 탑 명시 되어있는 거
-      categorysql =
-        'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 1 ORDER BY b_time';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = 1 ORDER BY b_time desc limit ' +
-        j +
-        ', 1)';
-      break;
-    case 3: //pants
-      categorysql =
-        'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 2 ORDER BY b_time';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = 2 ORDER BY b_time desc limit ' +
-        j +
-        ', 1)';
-      break;
-    case 4: //outer
-      categorysql =
-        'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 3 ORDER BY b_time';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = 3 ORDER BY b_time desc limit ' +
-        j +
-        ', 1)';
-      break;
-    case 5: //skirt
-      categorysql =
-        'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 4 ORDER BY b_time';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = 4 ORDER BY b_time desc limit ' +
-        j +
-        ', 1)';
-      break;
-    case 6: //shoes&bags
-      categorysql =
-        'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = 5 ORDER BY b_time';
-      colorsql =
-        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = 5 ORDER BY b_time desc limit ' +
-        j +
-        ', 1)';
-      break;
+    case 2:
+      category = 1;
+    case 3:
+      category = 2;
+    case 4:
+      category = 3;
+    case 5:
+      category = 4;
+    case 6:
+      category = 5;
+  }
+
+  if (categoryid == 0 || 1) {
+    categorysql =
+      'SELECT DISTINCT b.b_name, b.b_url, b.b_price, b.b_views, b.b_time FROM board b, board_color bc WHERE b.b_num = bc.bc_num ORDER BY b.' +
+      sort +
+      ' ' +
+      standard +
+      ' limit ' +
+      (page - 1) * size +
+      ', ' +
+      size;
+  } else {
+    categorysql =
+      'select DISTINCT b.b_name, b.b_url, b.b_price, b.b_views, b.b_time from board b, board_color bc where b.b_num = bc.bc_num and b.c_num = ' +
+      category +
+      ' ORDER BY b.' +
+      sort +
+      ' ' +
+      standard +
+      ' limit ' +
+      (page - 1) * size +
+      ', ' +
+      size;
   }
 
   let sqlresult = { data1: [] };
-  let jcount = 0;
 
   db.query(categorysql, (err, result) => {
     if (err) console.log(err);
     else {
       sqlresult.data1.push(...result);
-      jcount = result.length;
-
-      console.log(result);
-      for (let j = 0; j < jcount; j++) {
-        console.log(j);
+      let count = 0;
+      for (let q = (page - 1) * size; q <= size; q++) {
+        if (categoryid == 0 || 1) {
+          colorsql =
+            'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board ORDER BY ' +
+            sort +
+            ' ' +
+            standard +
+            ' limit ' +
+            q +
+            ', 1)';
+        } else {
+          colorsql =
+            'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = ' +
+            category +
+            ' ORDER BY ' +
+            sort +
+            ' ' +
+            standard +
+            ' limit ' +
+            q +
+            ', 1)';
+        }
         let semi = [];
         let dummy = [];
         db.query(colorsql, (err, secondresult) => {
@@ -100,14 +104,11 @@ app.get('/:categoryid', (req, res, next) => {
             for (let data of secondresult) {
               semi.push(data);
             }
-            console.log(secondresult);
-            console.log(j);
-            sqlresult.data1[j].b_color = dummy.concat(...semi);
+            sqlresult.data1[count].b_color = dummy.concat(...semi);
           }
-          console.log(jcount + ' 두번');
-
-          if (j == jcount - 1) res.send(sqlresult);
+          if (q == size) res.send(sqlresult);
         });
+        count++;
       }
     }
   });

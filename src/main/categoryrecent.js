36476692ab4ef38b2a401500f60e82708e514c2c
qsc0415 <qsc0415@naver.com>
2022-09-29 04:03:31 +0900

    Update categoryrecent.js

--- src/main/categoryrecent.js
+++ src/main/categoryrecent.js
@@ -83,50 +83,50 @@ app.get('/', (req, res, next) => {
     if (err) console.log(err);
     else {
       sqlresult.data1.push(...result);
-      let count = 0;
-      for (let q = (page - 1) * size; q <= size; q++) {
-        if (categoryid == 0 || categoryid == 1) {
-          colorsql =
-            'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board ORDER BY ' +
-            sort +
-            ' ' +
-            standard +
-            ' limit ' +
-            q +
-            ', 1)';
-        } else {
-          colorsql =
-            'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = ' +
-            category +
-            ' ORDER BY ' +
-            sort +
-            ' ' +
-            standard +
-            ' limit ' +
-            q +
-            ', 1)';
-        }
-        console.log(colorsql);
-        console.log(count);
-        let semi = [];
-        let dummy = [];
-        console.log(sqlresult);
-
-        db.query(colorsql, (err, secondresult) => {
-          if (err) console.log(err);
-          else {
-            for (let data of secondresult) {
-              semi.push(data);
-            }
-            console.log(...semi);
-            sqlresult.data1[count].b_color = dummy.concat(...semi);
-          }
-          if (q == size) res.send(sqlresult);
-        });
-        count++;
-      }
     }
   });
+  let count = 0;
+  for (let q = (page - 1) * size; q <= size; q++) {
+    if (categoryid == 0 || categoryid == 1) {
+      colorsql =
+        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board ORDER BY ' +
+        sort +
+        ' ' +
+        standard +
+        ' limit ' +
+        q +
+        ', 1)';
+    } else {
+      colorsql =
+        'SELECT bc.b_color FROM board b, board_color bc WHERE bc.bc_num = b.b_num AND bc.bc_num = (SELECT b_num FROM board where c_num = ' +
+        category +
+        ' ORDER BY ' +
+        sort +
+        ' ' +
+        standard +
+        ' limit ' +
+        q +
+        ', 1)';
+    }
+    console.log(colorsql);
+    console.log(count);
+    let semi = [];
+    let dummy = [];
+    console.log(sqlresult);
+
+    db.query(colorsql, (err, secondresult) => {
+      if (err) console.log(err);
+      else {
+        for (let data of secondresult) {
+          semi.push(data);
+        }
+        console.log(...semi);
+        sqlresult.data1[count].b_color = dummy.concat(...semi);
+      }
+      if (q == size) res.send(sqlresult);
+    });
+    count++;
+  }
 });
 
 module.exports = app;

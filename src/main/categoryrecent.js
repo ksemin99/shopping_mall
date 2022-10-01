8db5368d8886b3c755000fbe537939f9de911ed2
qsc0415 <qsc0415@naver.com>
2022-10-01 16:49:41 +0900

    Update categoryrecent.js

--- src/main/categoryrecent.js
+++ src/main/categoryrecent.js
@@ -76,7 +76,7 @@ app.get('/', (req, res, next) => {
         newsize = sqlcount[0].total % size;
       } else {
         newsize = size;
-        console.log(Number(newsize));
+        console.log(Number(newsize + ''));
       }
       if (search != undefined) {
         categorysql =

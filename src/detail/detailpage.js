bd38541353c20327428191ff2d46ffc2324aa921
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-10-17 15:21:53 +0900

    Update detailpage.js

--- src/detail/detailpage.js
+++ src/detail/detailpage.js
@@ -13,6 +13,8 @@ app.use(cors());
 dotenv.config();
 
 app.get('/:pagenum', (req, res, next) => {
+
+    let sqlresult = { data1: [] };
     const boardnum = req.params.pagenum;
     let board_c_num;
     c_numsql = 'SELECT c_num FROM board WHERE b_num = ' + boardnum
@@ -20,39 +22,42 @@ app.get('/:pagenum', (req, res, next) => {
     b_numsql = 'SELECT * FROM board WHERE b_num = ' + boardnum
     detailpicturesql = 'SELECT b_detail_picture_url FROM board_detail_picture WHERE b_num = ' + boardnum
     detailsql = 'SELECT b_detail_url FROM board_detail WHERE b_num = ' + boardnum
-
-    db.query(c_numsql, (err, c_numresult) => {
-        if (err) console.log(err);
-        else {
-
-            console.log(c_numresult[0].c_num);
-
-            board_c_num = parseInt(c_numresult[0].c_num);
-            switch (board_c_num) {
-                case 1: sizesql = 'SELECT b_size, outseam, shoulder, across, sleeve, muscle, hem FROM board_size WHERE b_num = ' + boardnum
-                    break;
-
-                case 2: sizesql = 'SELECT b_size, outseam, waist, hip, rise, thigh, hem FROM board_size WHERE b_num = ' + boardnum
-                    break;
-
-                case 3: sizesql = 'SELECT b_size, outseam, shoulder, across, armhole, hem FROM board_size WHERE b_num = ' + boardnum
-                    break;
-
-                case 4: sizesql = 'SELECT b_size, outseam, waist, hip, hem FROM board_size WHERE b_num = ' + boardnum
-                    break;
-
-                case 5: sizesql = 'SELECT b_size, height, feet, heel FROM board_size WHERE b_num = ' + boardnum
-                    break;
-            }
-            db.query(sizesql, (err, sizeresult) => {
-                if (err) console.log(err);
-                else {
-                    res.send(sizeresult);
-                }
-            });
-            //res.send(c_numresult);
-        }
+    db.query(b_numsql, (err, result) => {
+        sqlresult.data1.push(...result);
+        console.log(sqlresult)
     });
+    // db.query(c_numsql, (err, c_numresult) => {
+    //     if (err) console.log(err);
+    //     else {
+
+    //         console.log(c_numresult[0].c_num);
+
+    //         board_c_num = parseInt(c_numresult[0].c_num);
+    //         switch (board_c_num) {
+    //             case 1: sizesql = 'SELECT b_size, outseam, shoulder, across, sleeve, muscle, hem FROM board_size WHERE b_num = ' + boardnum
+    //                 break;
+
+    //             case 2: sizesql = 'SELECT b_size, outseam, waist, hip, rise, thigh, hem FROM board_size WHERE b_num = ' + boardnum
+    //                 break;
+
+    //             case 3: sizesql = 'SELECT b_size, outseam, shoulder, across, armhole, hem FROM board_size WHERE b_num = ' + boardnum
+    //                 break;
+
+    //             case 4: sizesql = 'SELECT b_size, outseam, waist, hip, hem FROM board_size WHERE b_num = ' + boardnum
+    //                 break;
+
+    //             case 5: sizesql = 'SELECT b_size, height, feet, heel FROM board_size WHERE b_num = ' + boardnum
+    //                 break;
+    //         }
+    //         db.query(sizesql, (err, sizeresult) => {
+    //             if (err) console.log(err);
+    //             else {
+    //                 res.send(sizeresult);
+    //             }
+    //         });
+    //         //res.send(c_numresult);
+    //     }
+    // });
 
 
 });

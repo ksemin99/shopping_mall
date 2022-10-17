1fadc9c91c4d2b6ebad7c2644e17ff7c33c693b9
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-10-17 15:05:39 +0900

    Update detailpage.js

--- src/detail/detailpage.js
+++ src/detail/detailpage.js
@@ -14,27 +14,47 @@ dotenv.config();
 
 app.get('/:pagenum', (req, res, next) => {
     const boardnum = req.params.pagenum;
+    let board_c_num;
     c_numsql = 'SELECT c_num FROM board WHERE b_num = ' + boardnum
-
-    db.query(c_numsql, (err, detailresult) => {
-        if (err) console.log(err);
-        else {
-            console.log(detailresult)
-            console.log(detailresult[0].c_num)
-            res.send(detailresult);
-        }
-    });
-
+    let sizesql;
     b_numsql = 'SELECT * FROM board WHERE b_num = ' + boardnum
     detailpicturesql = 'SELECT b_detail_picture_url FROM board_detail_picture WHERE b_num = ' + boardnum
     detailsql = 'SELECT b_detail_url FROM board_detail WHERE b_num = ' + boardnum
-    sizesql = 'SELECT * FROM board_size WHERE * is not null and b_num = ' + boardnum
-    // db.query(sizesql, (err, detailresult) => {
-    //     if (err) console.log(err);
-    //     else {
-    //         res.send(detailresult);
-    //     }
-    // });
+
+    db.query(c_numsql, (err, c_numresult) => {
+        if (err) console.log(err);
+        else {
+
+            console.log(c_numresult[0].c_num);
+
+            board_c_num = parseInt(c_numresult[0].c_num);
+            switch (board_c_num) {
+                case 1: sizesql = 'SELECT outseam, shoulder, across, sleeve, muscle, hem FROM board_size WHERE b_num = ' + boardnum
+                    break;
+
+                case 2: sizesql = 'SELECT outseam, waist, hip, rise, thigh, hem FROM board_size WHERE b_num = ' + boardnum
+                    break;
+
+                case 3: sizesql = 'SELECT outseam, shoulder, across, armhole, hem FROM board_size WHERE b_num = ' + boardnum
+                    break;
+
+                case 4: sizesql = 'SELECT outseam, waist, hip, hem FROM board_size WHERE b_num = ' + boardnum
+                    break;
+
+                case 5: sizesql = 'SELECT height, feet, heel FROM board_size WHERE b_num = ' + boardnum
+                    break;
+            }
+            db.query(sizesql, (err, sizeresult) => {
+                if (err) console.log(err);
+                else {
+                    res.send(sizeresult);
+                }
+            });
+            //res.send(c_numresult);
+        }
+    });
+
+
 });
 
 module.exports = app;

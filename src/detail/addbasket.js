291394f84b6df27d8b7b23399c398a19628de26c
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-10-24 17:48:05 +0900

    Update addbasket.js

--- src/detail/addbasket.js
+++ src/detail/addbasket.js
@@ -3,7 +3,6 @@ const app = express();
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const cors = require('cors');
-document
 
 // const mysqlConObj = require('../../config/mysql'); // #2
 // const { request } = require('express');
@@ -14,22 +13,22 @@ app.use(cors());
 dotenv.config();
 
 app.get('/', (req, res, next) => {
-    let test = "test"
-    let test1 = "test1"
-    let getCookie = function (name) {
-        var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    // let test = "test"
+    // let test1 = "test1"
+    // let getCookie = function (name) {
+    //     var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
 
-        return value ? value[2] : null;
-    };
-    function setCookie(cName, cValue, cDay) {
-        let expire = new Date();
-        expire.setDate(expire.getDate() + cDay);
-        cookies = cName + '=' + escape(cValue) + '; path=/ '; // 한글 깨짐을 막기위해 escape(cValue)를 합니다.
-        if (typeof cDay != 'undefined') cookies += ';expires=' + expire.toGMTString() + ';';
-        document.cookie = cookies;
-    }
-    setCookie(test, test1, 1);
-    console.log(test)
+    //     return value ? value[2] : null;
+    // };
+    // function setCookie(cName, cValue, cDay) {
+    //     let expire = new Date();
+    //     expire.setDate(expire.getDate() + cDay);
+    //     cookies = cName + '=' + escape(cValue) + '; path=/ '; // 한글 깨짐을 막기위해 escape(cValue)를 합니다.
+    //     if (typeof cDay != 'undefined') cookies += ';expires=' + expire.toGMTString() + ';';
+    //     document.cookie = cookies;
+    // }
+    // setCookie(test, test1, 1);
+    // console.log(test)
 });
 
 module.exports = app;

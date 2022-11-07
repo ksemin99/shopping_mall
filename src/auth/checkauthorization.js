5340a738972f38b56671927386b6fc065cc6870c
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-11-07 14:25:27 +0900

    a

--- src/auth/checkauthorization.js
+++ src/auth/checkauthorization.js
@@ -17,6 +17,19 @@ module.exports = {
     });
   },
 
+  authenticaterefreshToken: function authenticaterefreshToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const refreshToken = authHeader && authHeader.split(' ')[1];
+    if (refreshToken == null) return res.send('로그인을 하지 않은 상태입니다.');
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err) return res.send('리프레쉬 토큰이 만료되었습니다.');
+      const accessToken = checkauthorization.generateAccessToken({
+        id: user.id,
+        pw: user.pw,
+      });
+    });
+  },
+
   generateAccessToken: function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: '20s',
--- src/auth/token.js
+++ src/auth/token.js
@@ -10,24 +10,31 @@ const db = mysqlConObj.init();
 
 app.use(cors());
 
-app.post('/', (req, res) => {
-  const refreshToken = req.body.token;
-  if (refreshToken == null) return res.sendStatus(401);
-  // if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.send('리프레쉬 토큰이 만료되었습니다.');
-    const accessToken = checkauthorization.generateAccessToken({
-      id: user.id,
-      pw: user.pw,
-    });
+app.post('/', checkauthorization.authenticaterefreshToken, (req, res) => {
+  // const refreshToken = req.body.token;
+  // if (refreshToken == null) return res.sendStatus(401);
+  // // if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  // jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+  //   if (err) return res.send('리프레쉬 토큰이 만료되었습니다.');
+  //   const accessToken = checkauthorization.generateAccessToken({
+  //     id: user.id,
+  //     pw: user.pw,
+  //   });
 
-    const accessTokenExpiresIn =
-      checkauthorization.checkAccessTokenExpiresIn(accessToken);
-    res.json({
-      grantType: 'bearer',
-      accessToken: accessToken,
-      accessTokenExpiresIn: accessTokenExpiresIn,
-    });
+  //   const accessTokenExpiresIn =
+  //     checkauthorization.checkAccessTokenExpiresIn(accessToken);
+  //   res.json({
+  //     grantType: 'bearer',
+  //     accessToken: accessToken,
+  //     accessTokenExpiresIn: accessTokenExpiresIn,
+  //   });
+  // });
+  const accessTokenExpiresIn =
+    checkauthorization.checkAccessTokenExpiresIn(accessToken);
+  res.json({
+    grantType: 'bearer',
+    accessToken: accessToken,
+    accessTokenExpiresIn: accessTokenExpiresIn,
   });
 });
 

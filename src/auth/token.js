ce4a9568d868f06f83428061b9df762f08903235
qsc0415 <qsc0415@naver.com>
2022-11-13 02:32:40 +0900

    Update token.js

--- src/auth/token.js
+++ src/auth/token.js
@@ -11,34 +11,43 @@ const db = mysqlConObj.init();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  const id = req.body.id
-  sql = 'select token from users where id = "' + id + '"';
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.send('로그인을 하지 않은 상태입니다.');
+  sql = 'select token from users where token = "' + token + '"';
   db.query(sql, (err, result) => {
     if (err) console.log(err);
     else {
-      let refreshToken = "";
+      let refreshToken = '';
       for (let data of result) {
-        refreshToken = data.token
-        console.log(refreshToken)
+        refreshToken = data.token;
+        console.log(refreshToken);
       }
-      if (refreshToken == null) return res.send('로그인을 하지 않은 상태입니다.');
-      jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.send('리프레쉬 토큰이 만료되었습니다. 다시 로그인');
-        const accessToken = checkauthorization.generateAccessToken({  // refreshToken이 살아있을 때
-          id: user.id,
-          pw: user.pw,
-        });
-        const accessTokenExpiresIn =
-          checkauthorization.checkAccessTokenExpiresIn(accessToken);  // accessToken 생명주기 불러오기
-        res.send = ({                                                    // accessToken 발급
-          grantType: 'bearer',
-          accessToken: accessToken,
-          accessTokenExpiresIn: accessTokenExpiresIn,
-        });
-      });
+      if (refreshToken == null)
+        return res.send('로그인을 하지 않은 상태입니다.');
+      jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, user) => {
+          if (err)
+            return res.send('리프레쉬 토큰이 만료되었습니다. 다시 로그인');
+          const accessToken = checkauthorization.generateAccessToken({
+            // refreshToken이 살아있을 때
+            id: user.id,
+            pw: user.pw,
+          });
+          const accessTokenExpiresIn =
+            checkauthorization.checkAccessTokenExpiresIn(accessToken); // accessToken 생명주기 불러오기
+          res.send = {
+            // accessToken 발급
+            grantType: 'bearer',
+            accessToken: accessToken,
+            accessTokenExpiresIn: accessTokenExpiresIn,
+          };
+        }
+      );
     }
   });
-
 });
 
 module.exports = app;

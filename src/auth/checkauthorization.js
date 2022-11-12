2a9535b394b56ceda16cf2661307056cc9695f54
qsc0415 <qsc0415@naver.com>
2022-11-13 03:31:00 +0900

    Update checkauthorization.js

--- src/auth/checkauthorization.js
+++ src/auth/checkauthorization.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  authenticateToken: function authenticateToken(req, res, next) { //로그아웃시 access토큰 받기
+  authenticateToken: function authenticateToken(req, res, next) {
+    //로그아웃시 access토큰 받기
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.send('로그인을 하지 않은 상태입니다.');
@@ -11,20 +12,24 @@ module.exports = {
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       //accesstoken 받고 .env에 있는 토큰 암호값 계산해서 token에 맞는 사용자 정보 가져오기
-      if (err) res.send('AccessToken이 만료되었거나 틀린 AccessToken /auth/token 으로 RefreshToken 요함');
+      if (err)
+        res.send(
+          'AccessToken이 만료되었거나 틀린 AccessToken /auth/token 으로 RefreshToken 요함'
+        );
       else req.user = user;
       next();
     });
   },
 
-
-  generateAccessToken: function generateAccessToken(user) {       // accesstoken 생성
+  generateAccessToken: function generateAccessToken(user) {
+    // accesstoken 생성
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: '20s',
     });
   },
 
-  generateRefreshToken: function generateRefreshToken(user) {       // refreshtoken 생성
+  generateRefreshToken: function generateRefreshToken(user) {
+    // refreshtoken 생성
     return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
       expiresIn: '1d',
     });
@@ -35,7 +40,7 @@ module.exports = {
     let result = 0;
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) return res.sendStatus(403);
-      result = user.exp - user.iat;
+      result = (user.exp - user.iat) * 1000;
     });
     return result;
   },
@@ -47,7 +52,7 @@ module.exports = {
     let result = 0;
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
       if (err) return res.sendStatus(403);
-      result = user.exp - user.iat;
+      result = (user.exp - user.iat) * 1000;
     });
     return result;
   },
@@ -66,5 +71,4 @@ module.exports = {
   //     next();
   //   });
   // },
-
 };

6fca6a773c8f79604e106572f6d1de6ee838aa6d
qsc0415 <qsc0415@naver.com>
2022-12-02 04:30:40 +0900

    Update addcomment.js

--- src/detail/comment/addcomment.js
+++ src/detail/comment/addcomment.js
@@ -37,7 +37,7 @@ app.post(
     const u_num = Number(req.body.u_num);
     const id = req.body.id;
     const comment = req.body.comment;
-    const img = `img/${req.file.filename}`;
+    const img = '!'; //`img/${req.file.filename}`;
 
     comment_sql =
       "insert into comment(b_num, u_num, id, comment, c_img) values('" +

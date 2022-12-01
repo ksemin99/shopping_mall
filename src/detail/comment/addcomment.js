70da476d3e79846013fa6c264a594ae420f37b10
qsc0415 <qsc0415@naver.com>
2022-12-02 05:01:28 +0900

    Update addcomment.js

--- src/detail/comment/addcomment.js
+++ src/detail/comment/addcomment.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
     cb(null, 'img');
   },
   filename: function (req, file, cb) {
-    cb(null, file.filename + '-' + Date.now());
+    cb(null, file.fieldname + '-' + Date.now());
   },
 });
 
@@ -37,7 +37,7 @@ app.post(
     const u_num = Number(req.body.u_num);
     const id = req.body.id;
     const comment = req.body.comment;
-    const img = `img/${req.file.filename}`;
+    const img = `img/${req.file.filename}`; // 필수 아니게 바꾸기
 
     comment_sql =
       "insert into comment(b_num, u_num, id, comment, c_img) values('" +

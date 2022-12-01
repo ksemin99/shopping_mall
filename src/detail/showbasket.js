95778d7587625430a02bdb9cc9d714a11cc99cdf
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-12-01 11:10:53 +0900

    Update showbasket.js

--- src/detail/showbasket.js
+++ src/detail/showbasket.js
@@ -14,7 +14,9 @@ app.use(cookieParser('secretKey'))
 app.use(cors());
 
 dotenv.config();
-
+// b_num을 이용해 b_url 사진 주소 가져오기, 
+// b_num을 이용해 b_name 상품이름 가져오기, 
+// b_num을 이용해 b_price 상품가격 가져오기
 app.get('/', (req, res, next) => {
     const id = req.query.id
     const cookie = req.signedCookies.key

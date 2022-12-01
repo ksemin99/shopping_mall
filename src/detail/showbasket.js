213fa25e4ada3bfac1960e29bc0c6705caa125bd
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-12-01 16:15:39 +0900

    Update showbasket.js

--- src/detail/showbasket.js
+++ src/detail/showbasket.js
@@ -30,7 +30,7 @@ app.get('/', (req, res, next) => {
     db.query(showbasketsql, (err, showbasketresult) => {
         if (err) console.log(err);
         else {
-            res.send(showbasketresult[1].key)
+            res.send(showbasketresult[1].value)
         }
     });
 });

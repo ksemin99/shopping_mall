c40f6cc429a6527dd1720092ecbfaa34431fd359
qsc0415 <qsc0415@naver.com>
2022-12-02 04:29:19 +0900

    Update addcomment.js

--- src/detail/comment/addcomment.js
+++ src/detail/comment/addcomment.js
@@ -14,45 +14,50 @@ app.use(cors());
 
 dotenv.config();
 
-const multer = require("multer");
+const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'img')
+    cb(null, 'img');
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.filename + '-' + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post(
+  '/',
+  checkauthorization.authenticateToken,
+  upload.single('img'),
+  (req, res, next) => {
+    console.log(req.file);
+    const b_num = Number(req.body.b_num);
+    const u_num = Number(req.body.u_num);
+    const id = req.body.id;
+    const comment = req.body.comment;
+    const img = `img/${req.file.filename}`;
+
+    comment_sql =
+      "insert into comment(b_num, u_num, id, comment, c_img) values('" +
+      b_num +
+      "','" +
+      u_num +
+      "','" +
+      id +
+      "','" +
+      comment +
+      "','" +
+      img +
+      "');";
+    db.query(comment_sql, (err, comment_sqlresult) => {
+      if (err) console.log(err);
+      else {
+        console.log('추가 성공');
+      }
+    });
   }
-});
-
-const upload = multer({ storage: storage })
-
-app.post('/', checkauthorization.authenticateToken, upload.single('img'), (req, res, next) => {
-  console.log(req.file);
-  const b_num = Number(req.body.b_num);
-  const u_num = Number(req.body.u_num);
-  const id = req.body.id;
-  const comment = req.body.comment;
-  const img = `img/${req.file.filename}`;
-
-  comment_sql =
-    "insert into comment(b_num, u_num, id, comment, c_img) values('" +
-    b_num +
-    "','" +
-    u_num +
-    "','" +
-    id +
-    "','" +
-    comment +
-    "','" +
-    img +
-    "');";
-  db.query(comment_sql, (err, comment_sqlresult) => {
-    if (err) console.log(err);
-    else {
-      console.log('추가 성공');
-    }
-  });
-});
+);
 
 module.exports = app;

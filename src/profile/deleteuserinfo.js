61a8bc968cd44796a7aaeffbf03bfde0944c06a2
qsc0415 <qsc0415@naver.com>
2022-12-04 15:45:56 +0900

    q

--- src/profile/deleteuserinfo.js
+++ src/profile/deleteuserinfo.js
@@ -7,16 +7,24 @@ const cors = require('cors');
 const mysqlConObj = require('../../config/mysql'); // #2
 const { request } = require('express');
 const db = mysqlConObj.init();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
-app.use(cookieParser('secretKey'))
+app.use(cookieParser('secretKey'));
 
 app.use(cors());
 
 dotenv.config();
 
 app.get('/', (req, res, next) => {
-
+  const id = req.body.id;
+  // const pw = req.body.pw;
+  deletesql = 'DELETE FROM users WHERE id = ' + id;
+  db.query(deletesql, (err, deleteresult) => {
+    if (err) console.log(err);
+    else {
+      res.send(deletesql);
+    }
+  });
 });
 
 module.exports = app;
--- src/profile/updateuserinfo.js
+++ src/profile/updateuserinfo.js
@@ -7,38 +7,39 @@ const cors = require('cors');
 const mysqlConObj = require('../../config/mysql'); // #2
 const { request } = require('express');
 const db = mysqlConObj.init();
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
-app.use(cookieParser('secretKey'))
+app.use(cookieParser('secretKey'));
 
 app.use(cors());
 
 dotenv.config();
 
 app.patch('/', (req, res, next) => {
-    // 이름, 아이디, 비밀번호, 비밀번호 확인, 생년월일, 우편번호, 집주소, 상세주소, 이메일, 휴대폰, 계좌
-    const name = req.body.name;
-    const id = req.body.id;
-    const sex = req.body.sex;
-    const height = req.body.height;
-    const weight = req.body.weight;
+  // 이름, 아이디, 비밀번호, 비밀번호 확인, 생년월일, 우편번호, 집주소, 상세주소, 이메일, 휴대폰, 계좌
+  const name = req.body.name;
+  const id = req.body.id;
+  const sex = req.body.sex;
+  const height = req.body.height;
+  const weight = req.body.weight;
 
-    updatesql = 'UPDATE users SET name = ' +
-        name +
-        ', sex = ' +
-        sex +
-        ', height = ' +
-        height +
-        ', weigth = ' +
-        weight +
-        ' WHERE id = ' +
-        id;
-    db.query(updatesql, (err, updateresult) => {
-        if (err) console.log(err);
-        else {
-            res.send(name)
-        }
-    });
+  updatesql =
+    'UPDATE users SET name = ' +
+    name +
+    ', sex = ' +
+    sex +
+    ', height = ' +
+    height +
+    ', weigth = ' +
+    weight +
+    ' WHERE id = ' +
+    id;
+  db.query(updatesql, (err, updateresult) => {
+    if (err) console.log(err);
+    else {
+      res.send(updatesql);
+    }
+  });
 });
 
 module.exports = app;
